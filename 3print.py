cfc358eb37d6e002549d26f9688cff337b4f770b
Anuj Singh <57063408+anuj7052@users.noreply.github.com>
2024-05-15 18:27:27 +0530

    Add files via upload

---
+++ 3print.py
@@ -0,0 +1,17 @@
+print("********************")
+print("*"*20)
+
+
+print("#",end= " ")
+print("#",end= " ")
+print("#",end= " ")
+print("#")
+print("hello guys","how are you","where are you from ?",sep=" 12")
+
+
+print(11 & 8)
+print(~10)
+print(20^30)
+print(30&40|25^17)
+
+print(10>>2)
