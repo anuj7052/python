1ce65db73153922acbc58682394606b404eb0f91
Anuj Singh <57063408+anuj7052@users.noreply.github.com>
2024-05-22 18:51:51 +0530

    Add files via upload

---
+++ 12string.py
@@ -0,0 +1,12 @@
+Name = "Anuj"
+Age = 32
+marks = 100.00
+#string formating 1st methods
+print("My name is %s and my age is %d and i earn %0.2f marks"%(Name,Age,marks))
+
+#string formating 2st methods
+print("my name is {} and my age is{} and i earn {} marks".format(Name,Age,marks))
+
+#string formating 3st methods
+
+print(f"my name is {Name} my age is {Age} and i earn {marks} marks")
---
+++ 14 string slicing.py
@@ -0,0 +1,22 @@
+a = "my name is anuj"
+#b = a[start:end:step]
+b = a[3:7:1]
+print(b)
+
+
+
+a = "my name is anuj"
+#b = a[start:end:step]
+b = a[-12:-8:1]
+print(b)
+
+a = "my name is anuj"
+#b = a[start:end:step]
+b = a[-1:-16:-1]
+print(b)
+
+
+a = "my name is anuj"
+#b = a[start:end:step]
+b = (a[::-1])
+print(b)
