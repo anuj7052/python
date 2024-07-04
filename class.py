df87742963e3efa3a380a1e8805f87599527a26b
Anuj Singh <57063408+anuj7052@users.noreply.github.com>
2024-07-04 18:44:50 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,134 @@
+##class A:
+##    def show (self):
+##        name='anuj'
+##        age=21
+##        print(name,age)
+##    def display(self):
+##        email='dhfgkhfsgs'
+##        branch='hfksdj'
+##        print(email,branch)
+##        
+##a=A()
+##a.show()
+##a.display()
+
+
+##class A:
+##    def show (self,roll_no):
+##        print(roll_no)
+##        name='anuj'
+##        age=21
+##        print(name,age)
+##    def display(self):
+##        email='dhfgkhfsgs'
+##        branch='hfksdj'
+##        print(email,branch)
+##        
+##a=A()
+##a.show(123)
+##a.display()
+
+
+##class A:
+##    def show (self,name,age,salary):
+##        a.name1=name
+##        self.age1=age
+##        self.salary1=salary
+##        print(a.name1,self.age1,self.salary1)
+##
+##    def display(self):
+##        print(a.name1,self.age1,self.salary1)
+##
+##a=A()
+##a.show('anuj',21,35000)
+##a.display()
+##
+##
+###static 
+##
+##class student:
+##    name='anuj'
+##    email='anujsingh705223@GMAIL.COM'
+##    roll_no=121
+##    
+##    def demo(self): 
+##        name='anuj'
+##        print(name)
+##    
+##s= student()
+##s.demo()
+##k=student()
+##k.demo()
+##
+##m=student()
+##m.demo()
+
+
+
+
+##class A:
+##    def show(self):
+##        self.name=input('Enter Your Name: ')
+##        self.age=int(input('Enter your Age: '))
+##        self.salary=int(input('Enter salary: '))
+##
+##    def display(self):
+##        print('My name is',self.name)
+##        print('My age is',self.age)
+##        print('salary is',self.salary)
+##
+##a=A()
+##a.show()
+##a.display()
+##
+##b=A()
+##b.show()
+##b.display()
+
+##class A:
+##    def show (self,name,address):
+##        print('python developer')
+##    def display(self):
+##        self.show('anuj','ghazipur')
+##        a.show('kedar','itvedant')
+##        print('java developer')
+##
+##a=A()
+##a.show('anuj','ghazipur')
+##a.display()
+
+
+class A:
+    a=10
+    b=20
+    def demo(s):
+        s.a=s.a+1
+        A.b=A.b+1
+        print(s.a,A.b)
+x=A()
+x.demo()
+y=A()
+y.demo()
+z=A()
+z.demo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
